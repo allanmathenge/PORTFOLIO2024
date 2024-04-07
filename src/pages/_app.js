ee5ce0b94a43308e43b043485610b49a31d1fba5
unknown <allanmathenge2.com>
2024-04-07 21:05:01 +0300

    framer motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 import { Montserrat } from "next/font/google";
 
@@ -9,8 +11,18 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${montserrat.variable} font-mont`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +9,25 @@ export default function Home() {
       <Head>
         <title>PORTFOLIO</title>
         <meta name="description" content="PORTFOLIO" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Home</main>
+      <main>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
+          accusantium obcaecati perspiciatis quasi deleniti modi sint explicabo
+          dignissimos, placeat molestias, accusamus exercitationem voluptate
+          corporis repudiandae qui excepturi at molestiae sit! Minus,
+          reiciendis. Esse ipsa dolores obcaecati quidem unde reprehenderit
+          facere praesentium enim itaque illo eaque sint deserunt magnam
+          voluptas molestiae, nostrum rem et placeat possimus ex, adipisci
+          veritatis aut culpa. Fugiat omnis est quibusdam aut quidem natus!
+          Inventore dolorum nobis quis blanditiis dignissimos fugiat odit. Earum
+          ex culpa ipsa nesciunt explicabo illo dolor quos, hic, perspiciatis
+          cupiditate, labore laboriosam omnis? Distinctio rem amet laboriosam
+          explicabo sed ratione sapiente sit pariatur quo, nemo harum nisi animi
+          cumque vero perspiciatis nobis exercitationem, eos dolore dolores
+          quisquam consequuntur, qui nihil cupiditate! Rem, quis?
+        </h1>
+      </main>
     </>
   );
 }
