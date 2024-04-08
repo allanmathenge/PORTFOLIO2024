897c876e78b27de76b466e58785cb05e5187a1db
unknown <allanmathenge2.com>
2024-04-08 20:50:59 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import Logo from "../components/Logo";
 import { useRouter } from "next/router";
-import TwitterIcon from "../components/Icons";
+import { motion } from "framer-motion";
+import {
+  TwitterIcon,
+  GithubIcon,
+  LinkedInIcon,
+  YouTubeIcon,
+} from "../components/Icons";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -25,27 +31,52 @@ const Navbar = () => {
       <nav>
         <CustomLink href="/" title="Home" className="mx-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
-        <CustomLink href="/projects" title="Pojects" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
         <CustomLink href="/articles" title="Articles" className="mx-4" />
       </nav>
 
-      <nav>
-        <Link href="/" target={"_blank"}>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://github.com/allanmathenge"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <GithubIcon />
+        </motion.a>
+
+        <motion.a
+          href="https://www.youtube.com/channel/UCZkAGHIat_CrbuZ9CBmNlww"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <YouTubeIcon />
+        </motion.a>
+
+        <motion.a
+          href="https://www.linkedin.com/in/allan-mathenge-2a6b51252/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BuyeBYGlgRciBVN4ggoUs5A%3D%3D"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+
+        <motion.a
+          href="https://twitter.com/1mathenge?t=Cz2Q9lopNxAODz76obsvng&s=09"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
           <TwitterIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
+        </motion.a>
       </nav>
+
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,24 +10,7 @@ export default function Home() {
         <title>PORTFOLIO</title>
         <meta name="description" content="PORTFOLIO" />
       </Head>
-      <main>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-          accusantium obcaecati perspiciatis quasi deleniti modi sint explicabo
-          dignissimos, placeat molestias, accusamus exercitationem voluptate
-          corporis repudiandae qui excepturi at molestiae sit! Minus,
-          reiciendis. Esse ipsa dolores obcaecati quidem unde reprehenderit
-          facere praesentium enim itaque illo eaque sint deserunt magnam
-          voluptas molestiae, nostrum rem et placeat possimus ex, adipisci
-          veritatis aut culpa. Fugiat omnis est quibusdam aut quidem natus!
-          Inventore dolorum nobis quis blanditiis dignissimos fugiat odit. Earum
-          ex culpa ipsa nesciunt explicabo illo dolor quos, hic, perspiciatis
-          cupiditate, labore laboriosam omnis? Distinctio rem amet laboriosam
-          explicabo sed ratione sapiente sit pariatur quo, nemo harum nisi animi
-          cumque vero perspiciatis nobis exercitationem, eos dolore dolores
-          quisquam consequuntur, qui nihil cupiditate! Rem, quis?
-        </h1>
-      </main>
+      <main></main>
     </>
   );
 }
