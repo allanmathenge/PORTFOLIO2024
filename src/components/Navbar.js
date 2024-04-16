f5650c21c0fa82f76851b16ee48dd65b6c52bbe7
unknown <allanmathenge2.com>
2024-04-17 02:40:10 +0300

    res home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="w-full px-32 py-8 dark:text-light font-medium flex items-center justify-between relative">
+    <header className="w-full px-32 py-8 dark:text-light font-medium flex items-center justify-between relative z-10 lg:px-16 md:px-12 sm:px-8">
       <button
         className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
