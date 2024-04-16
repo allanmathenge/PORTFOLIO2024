c1971b8d8ffd2cc10315c83858d5ae1a45920d3b
unknown <allanmathenge2.com>
2024-04-17 01:06:12 +0300

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,6 +29,32 @@ const CustomLink = ({ href, title, className = "" }) => {
   );
 };
 
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      onClick={handleClick}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+    >
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        } dark:bg-dark`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
 const Navbar = () => {
   const [mode, setMode] = useThemeSwitcher();
   const [isOpen, setIsOpen] = useState(false);
@@ -69,69 +95,87 @@ const Navbar = () => {
         </nav>
       </div>
 
-      <div className="min-w-[70vw] flex flex-col justify-between z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32 items-center left-1/2 fixed top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <nav>
-          <CustomLink href="/" title="Home" className="mx-4" />
-          <CustomLink href="/about" title="About" className="mx-4" />
-          <CustomLink href="/projects" title="Projects" className="mx-4" />
-          <CustomLink href="/articles" title="Articles" className="mx-4" />
-        </nav>
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col justify-between z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32 items-center left-1/2 fixed top-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink href="/" title="Home" toggle={handleClick} />
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/articles"
+              title="Articles"
+              toggle={handleClick}
+            />
+          </nav>
 
-        <nav className="flex items-center justify-center flex-wrap">
-          <motion.a
-            href="https://github.com/allanmathenge"
-            target={"_blank"}
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            className="w-6 mx-3"
-          >
-            <GithubIcon />
-          </motion.a>
+          <nav className="flex items-center justify-center flex-wrap mt-2">
+            <motion.a
+              href="https://github.com/allanmathenge"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1"
+            >
+              <GithubIcon />
+            </motion.a>
 
-          <motion.a
-            href="https://www.youtube.com/channel/UCZkAGHIat_CrbuZ9CBmNlww"
-            target={"_blank"}
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            className="w-6 mx-3"
-          >
-            <YouTubeIcon />
-          </motion.a>
+            <motion.a
+              href="https://www.youtube.com/channel/UCZkAGHIat_CrbuZ9CBmNlww"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <YouTubeIcon />
+            </motion.a>
 
-          <motion.a
-            href="https://www.linkedin.com/in/allan-mathenge-2a6b51252/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BuyeBYGlgRciBVN4ggoUs5A%3D%3D"
-            target={"_blank"}
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            className="w-6 mx-3"
-          >
-            <LinkedInIcon />
-          </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/allan-mathenge-2a6b51252/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BuyeBYGlgRciBVN4ggoUs5A%3D%3D"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <LinkedInIcon />
+            </motion.a>
 
-          <motion.a
-            href="https://twitter.com/1mathenge?t=Cz2Q9lopNxAODz76obsvng&s=09"
-            target={"_blank"}
-            whileHover={{ y: -2 }}
-            whileTap={{ scale: 0.9 }}
-            className="w-7 h-7 flex items-center justify-center ml-3 bg-light rounded-full"
-          >
-            <XIcon />
-          </motion.a>
+            <motion.a
+              href="https://twitter.com/1mathenge?t=Cz2Q9lopNxAODz76obsvng&s=09"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-7 h-7 flex items-center justify-center ml-3 bg-light rounded-full sm:mx-1"
+            >
+              <XIcon />
+            </motion.a>
 
-          <button
-            onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`ml-3 flex items-center rounded-full p-1 ${
-              mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
-            }`}
-          >
-            {mode === "dark" ? (
-              <SunIcon className={"fill-dark"} />
-            ) : (
-              <MoonIcon className={"fill-dark"} />
-            )}
-          </button>
-        </nav>
-      </div>
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center rounded-full p-1 ${
+                mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+              }`}
+            >
+              {mode === "dark" ? (
+                <SunIcon className={"fill-dark"} />
+              ) : (
+                <MoonIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </motion.div>
+      ) : null}
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
